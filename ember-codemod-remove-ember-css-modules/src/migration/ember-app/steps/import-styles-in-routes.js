f6bdd00f6f836849a55622730ec75693825d3251
ijlee2 <ijlee2@users.noreply.github.com>
2023-04-05 12:13:15 +0200

    feature: Defined how to create the class for routes

--- ember-codemod-remove-ember-css-modules/src/migration/ember-app/steps/import-styles-in-routes.js
+++ ember-codemod-remove-ember-css-modules/src/migration/ember-app/steps/import-styles-in-routes.js
@@ -1,6 +1,50 @@
-/* eslint-disable-next-line no-unused-vars */
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+import { blueprintRoot, processTemplate } from '../../../utils/blueprints.js';
+import { createFiles } from '../../../utils/files.js';
+import { parseEntityName } from '../../../utils/string.js';
+
+function getFilePath(entityName, options) {
+  const { project } = options;
+
+  let filePath = join('app/controllers', entityName);
+
+  if (project.hasTypeScript) {
+    filePath += '.ts';
+  } else {
+    filePath += '.js';
+  }
+
+  return filePath;
+}
+
 function createClass(entityName, options) {
-  // ...
+  const { project } = options;
+
+  const blueprintFilePaths = project.hasTypeScript
+    ? ['ember-cli/controller/typescript.ts']
+    : ['ember-cli/controller/javascript.js'];
+
+  const fileMapping = new Map(
+    blueprintFilePaths.map((blueprintFilePath) => {
+      const filePath = getFilePath(entityName, options);
+
+      const blueprintFile = readFileSync(
+        join(blueprintRoot, blueprintFilePath),
+        'utf8'
+      );
+
+      const file = processTemplate(blueprintFile, {
+        entity: parseEntityName(entityName),
+        options,
+      });
+
+      return [filePath, file];
+    })
+  );
+
+  createFiles(fileMapping, options);
 }
 
 /* eslint-disable-next-line no-unused-vars */
