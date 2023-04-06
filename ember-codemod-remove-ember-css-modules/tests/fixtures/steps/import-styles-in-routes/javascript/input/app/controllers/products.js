aa7460ad4ba154b99f5af8eb27b367382f83de97
ijlee2 <ijlee2@users.noreply.github.com>
2023-04-06 22:35:52 +0200

    chore: Updated test fixtures to match the code in docs-app better

--- ember-codemod-remove-ember-css-modules/tests/fixtures/steps/import-styles-in-routes/javascript/input/app/controllers/products.js
+++ ember-codemod-remove-ember-css-modules/tests/fixtures/steps/import-styles-in-routes/javascript/input/app/controllers/products.js
@@ -1,7 +1,37 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ProductsController extends Controller {
+  @tracked name = null;
 
-  // ...
+  get isPartOfNestProductDetailsExperiment() {
+    return true;
+  }
 
+  get filteredProducts() {
+    const { model: products, name } = this;
+
+    if (!name) {
+      return products;
+    }
+
+    const target = name.toLowerCase();
+
+    return products.filter((product) => {
+      const productName = (product.name ?? '').toLowerCase();
+
+      return productName.includes(target);
+    });
+  }
+
+  @action noOp({ key, value }) {
+    if (value === undefined || value === '') {
+      this[key] = null;
+
+      return;
+    }
+
+    this[key] = value;
+  }
 }
