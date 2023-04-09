bafd00f8fb8fc416ef41025fcd01b324f7d8a415
ijlee2 <ijlee2@users.noreply.github.com>
2023-04-09 23:02:12 +0200

    feature: Defined how to add the CSS entry point in app.{js,ts}

--- ember-codemod-remove-ember-css-modules/src/migration/ember-app/steps/index.js
+++ ember-codemod-remove-ember-css-modules/src/migration/ember-app/steps/index.js
@@ -3,6 +3,7 @@ export * from './create-options.js';
 export * from './import-styles-in-components.js';
 export * from './import-styles-in-routes.js';
 export * from './move-stylesheets.js';
+export * from './update-app-app-js.js';
 export * from './update-component-templates.js';
 export * from './update-package-json.js';
 export * from './update-route-templates.js';
