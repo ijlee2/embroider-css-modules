fe8f24a32e0798d81f5af5dc1215c63b4d59849c
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-02 08:39:02 +0200

    refactor: Introduced a glob pattern that excludes .css.d.js files (false positives)

--- docs/sample-v2-addon/rollup.config.mjs
+++ docs/sample-v2-addon/rollup.config.mjs
@@ -16,8 +16,7 @@ export default {
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
     addon.publicEntrypoints([
-      'components/navigation-menu-new.js',
-      'components/ui/page-new.js',
+      'components/**/!(*.css.d).js',
       'index.js',
       'template-registry.js',
     ]),
@@ -25,10 +24,7 @@ export default {
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports([
-      'components/navigation-menu-new.js',
-      'components/ui/page-new.js',
-    ]),
+    addon.appReexports(['components/**/!(*.css.d).js']),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
