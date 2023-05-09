57e2627ffb70dd770199ac8cd82f81990d7629cb
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-09 11:30:13 +0200

    bugfix: Removed dependency on embroider-css-modules

--- packages/embroider-css-modules-temporary/src/helpers/local-class-new.ts
+++ packages/embroider-css-modules-temporary/src/helpers/local-class-new.ts
@@ -1,3 +1,53 @@
-import LocalClassNewHelper from 'embroider-css-modules/helpers/local-class';
+import Helper from '@ember/component/helper';
+import { assert } from '@ember/debug';
 
-export default LocalClassNewHelper;
+type IndexSignatureParameter = string | number | symbol;
+
+type LocalClassName<T extends IndexSignatureParameter> = T;
+
+type Styles<T extends IndexSignatureParameter> = Record<
+  LocalClassName<T>,
+  string
+>;
+
+type MaybeLocalClassName<T extends IndexSignatureParameter> =
+  | LocalClassName<T>[]
+  | LocalClassName<T>
+  | undefined
+  | null;
+
+interface LocalClassNewHelperSignature<T extends IndexSignatureParameter> {
+  Args: {
+    Positional: [Styles<T>, ...MaybeLocalClassName<T>[]];
+  };
+  Return: string;
+}
+
+export default class LocalClassNewHelper<
+  T extends IndexSignatureParameter,
+> extends Helper<LocalClassNewHelperSignature<T>> {
+  compute(positional: LocalClassNewHelperSignature<T>['Args']['Positional']) {
+    const [styles, ...localClassNames] = positional;
+
+    assert('The styles object is undefined.', styles);
+
+    const classNames = localClassNames.reduce<string[]>(
+      (accumulator, localClassName) => {
+        if (localClassName === undefined || localClassName === null) {
+          return accumulator;
+        }
+
+        if (Array.isArray(localClassName)) {
+          accumulator.push(...localClassName.map((element) => styles[element]));
+        } else {
+          accumulator.push(styles[localClassName]);
+        }
+
+        return accumulator;
+      },
+      [],
+    );
+
+    return classNames.filter(Boolean).join(' ');
+  }
+}
