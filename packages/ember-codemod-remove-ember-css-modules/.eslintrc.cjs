e671e32fc949b88f104958dc9fbb7531486a433b
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-18 11:00:38 +0200

    refactor: Extended the shared configuration for eslint (Node, JavaScript)

--- packages/ember-codemod-remove-ember-css-modules/.eslintrc.cjs
+++ packages/ember-codemod-remove-ember-css-modules/.eslintrc.cjs
@@ -1,28 +1,7 @@
 'use strict';
 
+require('@shared-configs/eslint-config/patch');
+
 module.exports = {
-  root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 2018,
-    requireConfigFile: false,
-    sourceType: 'module',
-  },
-  plugins: ['simple-import-sort'],
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:n/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    curly: 'error',
-    'simple-import-sort/exports': 'error',
-    'simple-import-sort/imports': 'error',
-  },
-  env: {
-    es6: true,
-    node: true,
-  },
-  overrides: [],
+  extends: ['@shared-configs/eslint-config/node-javascript'],
 };
