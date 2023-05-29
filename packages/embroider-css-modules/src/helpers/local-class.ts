ca2de189798c22847db8ae8d9e8c6eedfd94adc6
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-29 12:37:42 +0200

    refactor: Introduced macros

--- packages/embroider-css-modules/src/helpers/local-class.ts
+++ packages/embroider-css-modules/src/helpers/local-class.ts
@@ -1,3 +1,13 @@
+import { dependencySatisfies, macroCondition } from '@embroider/macros';
+
 import EmbroiderCssModulesLocalClassHelper from './_local-class';
 
-export default EmbroiderCssModulesLocalClassHelper;
+const LocalClassHelper = (function () {
+  if (macroCondition(dependencySatisfies('ember-css-modules', '*'))) {
+    return undefined;
+  }
+
+  return EmbroiderCssModulesLocalClassHelper;
+})();
+
+export default LocalClassHelper;
