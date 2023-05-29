842a3eb6715830ea515d29a50f61bc58c20a7e31
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-29 13:29:24 +0200

    feature: Resolved the name collision between {{local-class}} helpers

--- packages/embroider-css-modules/src/helpers/local-class.ts
+++ packages/embroider-css-modules/src/helpers/local-class.ts
@@ -1,10 +1,34 @@
-import { dependencySatisfies, macroCondition } from '@embroider/macros';
+import {
+  dependencySatisfies,
+  importSync,
+  macroCondition,
+  moduleExists,
+} from '@embroider/macros';
+import { HelperLike } from '@glint/template';
 
 import EmbroiderCssModulesLocalClassHelper from './_local-class';
 
+type EmberCssModulesLocalClassHelper = HelperLike<{
+  Args: {
+    Named: {
+      from?: string;
+    };
+    Positional: [localClassNames: string];
+  };
+  Return: string;
+}>;
+
 const LocalClassHelper = (function () {
   if (macroCondition(dependencySatisfies('ember-css-modules', '*'))) {
-    return undefined;
+    if (moduleExists('ember-css-modules/helpers/local-class')) {
+      return importSync(
+        'ember-css-modules/helpers/local-class',
+      ) as EmberCssModulesLocalClassHelper;
+    }
+
+    throw new Error(
+      'Could not find the {{local-class}} helper from ember-css-modules.',
+    );
   }
 
   return EmbroiderCssModulesLocalClassHelper;
