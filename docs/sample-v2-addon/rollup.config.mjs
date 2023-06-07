77b3b396788170808b481a5546890c004bfa9060
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-06-07 09:03:13 +0200

    Updated dependencies (#52)
    * chore: Updated dependencies
    
    * chore: Updated @embroider packages
    
    * chore: Reverted the temporary patches
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- docs/sample-v2-addon/rollup.config.mjs
+++ docs/sample-v2-addon/rollup.config.mjs
@@ -10,16 +10,13 @@ const addon = new Addon({
 export default {
   // This provides defaults that work well alongside `publicEntrypoints` below.
   // You can augment this if you need to.
-  output: {
-    ...addon.output(),
-    experimentalMinChunkSize: 0,
-  },
+  output: addon.output(),
 
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
     addon.publicEntrypoints([
-      'components/**/!(*.css.d).js',
+      'components/**/*.js',
       'index.js',
       'template-registry.js',
     ]),
@@ -27,7 +24,7 @@ export default {
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['components/**/!(*.css.d).js']),
+    addon.appReexports(['components/**/*.js']),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
