80b63f0eff5a5b59fe9c6144cc0e410fceecb3ca
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-06-13 21:07:55 +0200

    Simplified project structure (#70)
    * chore: Moved blueprints back to src/blueprints
    
    * chore: Updated project (ember-codemod-remove-ember-css-modules)
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

---
+++ .changeset/giant-onions-camp.md
@@ -0,0 +1,5 @@
+---
+"ember-codemod-remove-ember-css-modules": patch
+---
+
+Simplified project structure
--- packages/ember-codemod-remove-ember-css-modules/src/utils/blueprints/blueprints-root.ts
+++ packages/ember-codemod-remove-ember-css-modules/src/utils/blueprints/blueprints-root.ts
@@ -4,7 +4,7 @@ import { getFilePath } from '@codemod-utils/blueprints';
 
 const fileURL = import.meta.url;
 
-export const blueprintsRoot = join(
+export const blueprintsRoot: string = join(
   getFilePath(fileURL),
-  '../../../../blueprints',
+  '../../blueprints',
 );
