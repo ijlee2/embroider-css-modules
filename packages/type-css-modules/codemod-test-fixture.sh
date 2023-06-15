c69ea7491c4fcb661fb8c2fcf739f2c3f728f0e9
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-15 20:21:09 +0200

    chore: Installed typescript

--- packages/type-css-modules/codemod-test-fixture.sh
+++ packages/type-css-modules/codemod-test-fixture.sh
@@ -48,7 +48,7 @@ fi
 rm -r "tests/fixtures/$FIXTURE/output"
 cp -r "tests/fixtures/$FIXTURE/input" "tests/fixtures/$FIXTURE/output"
 
-./bin/type-css-modules.js \
+./dist/bin/type-css-modules.js \
   --root="tests/fixtures/$FIXTURE/output" \
   --src=$SRC
 
