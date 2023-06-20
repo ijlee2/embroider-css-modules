24b6a42e0bae2eafbb73cf4fea947dab38a2962e
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-20 14:46:56 +0200

    chore: Added sample-v1-addon as a dependency of docs-for-embroider-css-modules

--- docs/embroider-css-modules/ember-cli-build.js
+++ docs/embroider-css-modules/ember-cli-build.js
@@ -11,7 +11,11 @@ module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
     autoImport: {
-      watchDependencies: ['embroider-css-modules', 'sample-v2-addon'],
+      watchDependencies: [
+        'embroider-css-modules',
+        'sample-v1-addon',
+        'sample-v2-addon',
+      ],
     },
     'ember-cli-babel': {
       enableTypeScriptTransform: true,
