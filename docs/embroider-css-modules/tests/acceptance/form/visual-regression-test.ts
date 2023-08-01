2cabf4cbd54fadeb5304671a3f7aee889f3c6848
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-08-01 15:10:33 +0200

    Updated dependencies (Part 1) (#92)
    * chore: Updated dependencies
    
    * chore: Added lockfile
    
    * chore: Fixed linting errors
    
    * chore: Replaced a private implementation with the public one
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- docs/embroider-css-modules/tests/acceptance/form/visual-regression-test.ts
+++ docs/embroider-css-modules/tests/acceptance/form/visual-regression-test.ts
@@ -7,8 +7,6 @@ module('Acceptance | form', function (hooks) {
   setupApplicationTest(hooks);
 
   test('Visual regression', async function (assert) {
-    assert.expect(1);
-
     await visit('/form');
     await takeSnapshot(assert);
 
