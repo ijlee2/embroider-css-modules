d79110cbf8c10a44414cd5a2aa52a744b9a911cd
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-09-14 16:47:29 +0200

    Updated latestVersions (#105)
    * chore: Updated latestVersions
    
    * chore: Updated test fixtures
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/ember-codemod-remove-ember-css-modules/src/utils/blueprints/get-version.ts
+++ packages/ember-codemod-remove-ember-css-modules/src/utils/blueprints/get-version.ts
@@ -3,15 +3,15 @@ import { decideVersion } from '@codemod-utils/blueprints';
 import type { Options } from '../../types/index.js';
 
 const latestVersions = new Map([
-  ['@embroider/compat', '3.0.2'],
-  ['@embroider/core', '3.0.2'],
-  ['@embroider/webpack', '3.0.0'],
-  ['autoprefixer', '10.4.14'],
-  ['embroider-css-modules', '0.1.9'],
-  ['postcss', '8.4.24'],
+  ['@embroider/compat', '3.2.1'],
+  ['@embroider/core', '3.2.1'],
+  ['@embroider/webpack', '3.1.5'],
+  ['autoprefixer', '10.4.15'],
+  ['embroider-css-modules', '1.0.0'],
+  ['postcss', '8.4.29'],
   ['postcss-loader', '7.3.3'],
-  ['type-css-modules', '0.2.2'],
-  ['webpack', '5.87.0'],
+  ['type-css-modules', '1.0.0'],
+  ['webpack', '5.88.2'],
 ]);
 
 export function getVersion(packageName: string, options: Options): string {
