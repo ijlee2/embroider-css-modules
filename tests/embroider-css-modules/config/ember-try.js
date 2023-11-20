3f6540b7bb69c1788691f23cf1585e7c59a22041
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-11-20 09:12:24 +0100

    Update dependencies (#120)
    * chore: Updated @ember/test-helpers to v3.0.3
    
    * chore: Added lockfile
    
    * chore: Updated dependencies
    
    * chore: Updated lockfile
    
    * chore: Updated ember-source to v5.4.0
    
    * chore: Updated lockfile
    
    * chore: Fixed linting errors
    
    * chore: Updated peer dependencies for shared configurations
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- tests/embroider-css-modules/config/ember-try.js
+++ tests/embroider-css-modules/config/ember-try.js
@@ -49,7 +49,6 @@ module.exports = async function () {
       },
       {
         name: 'ember-canary',
-        allowedToFail: true,
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
