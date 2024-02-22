9dee2bd0d3eeb9a1b4bbd7f96bd928d2969dbc3e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2024-02-22 08:42:11 +0100

    Updated dependencies (#129)
    * chore: Updated dependencies
    
    * chore: Added lockfile
    
    * chore: Updated ember-cli-update.json
    
    * refactor: Consumed ember-page-title's template registry
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- docs/my-app/types/my-app/index.d.ts
+++ docs/my-app/types/my-app/index.d.ts
@@ -3,8 +3,9 @@ import '@glint/environment-ember-template-imports';
 import 'ember-source/types';
 import 'ember-source/types/preview';
 
-import type { ComponentLike, HelperLike } from '@glint/template';
+import type { ComponentLike } from '@glint/template';
 import type EmberContainerQueryRegistry from 'ember-container-query/template-registry';
+import type EmberPageTitleRegistry from 'ember-page-title/template-registry';
 import type EmberSvgJarRegistry from 'ember-svg-jar/template-registry';
 import type EmberTruthHelpersRegistry from 'ember-truth-helpers/template-registry';
 import type EmbroiderCssModulesRegistry from 'embroider-css-modules/template-registry';
@@ -17,14 +18,10 @@ type NavigationNarratorComponent = ComponentLike<{
   };
 }>;
 
-type PageTitleHelper = HelperLike<{
-  Args: { Positional: [title: string] };
-  Return: void;
-}>;
-
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry
     extends EmberContainerQueryRegistry,
+      EmberPageTitleRegistry,
       EmberSvgJarRegistry,
       EmberTruthHelpersRegistry,
       EmbroiderCssModulesRegistry,
@@ -33,6 +30,5 @@ declare module '@glint/environment-ember-loose/registry' {
     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
     // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
     NavigationNarrator: NavigationNarratorComponent;
-    'page-title': PageTitleHelper;
   }
 }
