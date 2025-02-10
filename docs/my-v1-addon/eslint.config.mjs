5164a31e958576fd9174a08d776324563de78a03
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-10 12:07:43 +0100

    chore: Simplified project configurations

--- docs/my-v1-addon/eslint.config.mjs
+++ docs/my-v1-addon/eslint.config.mjs
@@ -1,3 +1,1 @@
-import eslintConfigEmberV1Addon from '@shared-configs/eslint-config-ember/v1-addon/index.js';
-
-export default eslintConfigEmberV1Addon;
+export { default } from '@shared-configs/eslint-config-ember/v1-addon';
