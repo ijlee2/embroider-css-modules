e4eacfe9e7339bf399793a5b21829d30ebe2d632
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-27 14:45:55 +0100

    feature: Implemented CSS modules with lightningcss

--- docs/my-v2-app/vite.config.mjs
+++ docs/my-v2-app/vite.config.mjs
@@ -3,10 +3,16 @@ import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    cssMinify: 'lightningcss',
+  },
   css: {
-    modules: {
-      generateScopedName: 'my-v2-app__[sha512:hash:base64:5]',
+    lightningcss: {
+      cssModules: {
+        pattern: 'my-v2-app__[hash]-[local]',
+      },
     },
+    transformer: 'lightningcss',
   },
   plugins: [
     classicEmberSupport(),
