3a6b2a426b1e89bef7fb0715d65a75be32c4e9b4
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-27 14:44:51 +0100

    feature: Set up embroider-css-modules

--- docs/my-v2-app/vite.config.mjs
+++ docs/my-v2-app/vite.config.mjs
@@ -3,6 +3,11 @@ import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  css: {
+    modules: {
+      generateScopedName: 'my-v2-app__[sha512:hash:base64:5]',
+    },
+  },
   plugins: [
     classicEmberSupport(),
     ember(),
