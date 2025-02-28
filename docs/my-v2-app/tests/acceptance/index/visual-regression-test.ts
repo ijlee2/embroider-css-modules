3dc2d6e498e7b26f894a2e0e5f2480a2230dad85
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-28 09:06:25 +0100

    chore: Asserted style

--- docs/my-v2-app/tests/acceptance/index/visual-regression-test.ts
+++ docs/my-v2-app/tests/acceptance/index/visual-regression-test.ts
@@ -9,6 +9,12 @@ module('Acceptance | index', function (hooks) {
   test('We can visit index', async function (assert) {
     await visit('/');
 
-    assert.dom('[data-test-hello-container]').hasClass(styles.container);
+    assert
+      .dom('[data-test-hello-container]')
+      .hasClass(styles.container)
+      .hasStyle({
+        'background-image':
+          'linear-gradient(300deg, rgb(189, 52, 254) 30%, rgb(65, 209, 255))',
+      });
   });
 });
