780673de0cb96442bc5cd9352b58dbc07ed1c01f
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-28 11:53:52 +0100

    chore: Implemented generateName()

--- packages/percy-ember-v2/src/test-support/percy-snapshot.ts
+++ packages/percy-ember-v2/src/test-support/percy-snapshot.ts
@@ -2,11 +2,37 @@ import utils from '@percy/sdk-utils';
 
 const alwaysRun = true;
 
-export async function percySnapshot(name: string): Promise<void> {
+type QunitAssert = {
+  test: {
+    module: {
+      name: string;
+    };
+    testName: string;
+  };
+};
+
+// Helper to generate a snapshot name from the test suite
+function generateName(qunitAssertOrName: object | string): string {
+  const isQunitAssert = typeof qunitAssertOrName !== 'string';
+
+  if (isQunitAssert) {
+    const { test } = qunitAssertOrName as unknown as QunitAssert;
+
+    return `${test.module.name} | ${test.testName}`;
+  }
+
+  return qunitAssertOrName;
+}
+
+export async function percySnapshot(
+  qunitAssertOrName: object | string,
+): Promise<void> {
   // Check if Percy is enabled
   if (!alwaysRun && !(await utils.isPercyEnabled())) {
     return;
   }
+
+  const name = generateName(qunitAssertOrName);
 
   console.log(name);
 }
