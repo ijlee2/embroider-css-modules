94328200ee14c37bef115bd5f216b92ac62630d7
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-11-21 18:21:35 +0100

    feature: Created step (create-css-module-files)

--- packages/ember-codemod-remove-global-styles/src/index.ts
+++ packages/ember-codemod-remove-global-styles/src/index.ts
@@ -1,9 +1,13 @@
-import { analyzeProject, createOptions } from './steps/index.js';
+import {
+  analyzeProject,
+  createCssModuleFiles,
+  createOptions,
+} from './steps/index.js';
 import type { CodemodOptions } from './types/index.js';
 
 export function runCodemod(codemodOptions: CodemodOptions): void {
   const options = createOptions(codemodOptions);
 
   const project = analyzeProject(options);
-  console.log(project);
+  createCssModuleFiles(project, options);
 }
---
+++ packages/ember-codemod-remove-global-styles/src/steps/create-css-module-files.ts
@@ -0,0 +1,81 @@
+import { existsSync, readFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+import { findTemplateTags } from '@codemod-utils/ast-template-tag';
+import { createFiles, findFiles } from '@codemod-utils/files';
+
+import type { Options, Project, Style } from '../types/index.js';
+import {
+  getClasses,
+  getModuleFilePath,
+  printStyles,
+} from '../utils/css/index.js';
+
+export function createCssModuleFiles(project: Project, options: Options): void {
+  const { projectRoot } = options;
+
+  const filePaths = findFiles('app/{components,templates}/**/*.{gjs,gts,hbs}', {
+    projectRoot,
+  });
+
+  const fileMap = new Map<string, string>();
+
+  filePaths.forEach((filePath) => {
+    const file = readFileSync(join(projectRoot, filePath), 'utf8');
+
+    const classes: string[] = [];
+    const errors: string[] = [];
+
+    if (filePath.endsWith('.hbs')) {
+      const output = getClasses(file);
+
+      classes.push(...output.classes);
+      errors.push(...output.errors);
+    } else {
+      const templateTags = findTemplateTags(file);
+
+      templateTags.forEach(({ contents }) => {
+        const output = getClasses(contents);
+
+        classes.push(...output.classes);
+        errors.push(...output.errors);
+      });
+    }
+
+    const classesSet = new Set(classes);
+
+    const localStyles = classes.reduce((accumulator, className) => {
+      const styles = project.classToStyles.get(className) ?? [];
+
+      const filteredStyles = styles.filter(({ classes }) => {
+        return classes.every((className) => classesSet.has(className));
+      });
+
+      accumulator.push(...filteredStyles);
+
+      return accumulator;
+    }, [] as Style[]);
+
+    if (localStyles.length === 0) {
+      return;
+    }
+
+    const cssModuleFilePath = getModuleFilePath(filePath);
+
+    let cssModuleFile = existsSync(join(projectRoot, cssModuleFilePath))
+      ? readFileSync(join(projectRoot, cssModuleFilePath), 'utf8')
+      : '';
+
+    cssModuleFile += `${printStyles(localStyles)}\n`;
+
+    fileMap.set(cssModuleFilePath, cssModuleFile);
+
+    if (errors.length > 0) {
+      console.warn(`WARNING: ${cssModuleFilePath} may be incorrect.`);
+      console.warn(errors.map((error) => `- ${error}`).join('\n'));
+      console.log();
+    }
+  });
+
+  createFiles(fileMap, { projectRoot });
+}
