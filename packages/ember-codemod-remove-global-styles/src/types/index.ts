6747c6ca21384a14ab0ab41ac0eb199700794a29
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-11-21 18:20:37 +0100

    feature: Created utility (get-class-to-styles)

--- packages/ember-codemod-remove-global-styles/src/types/index.ts
+++ packages/ember-codemod-remove-global-styles/src/types/index.ts
@@ -8,4 +8,24 @@ type Options = {
   src: string;
 };
 
-export type { CodemodOptions, Options };
+type ClassToStyles = Map<string, Style[]>;
+
+type Style = {
+  classes: string[];
+  location: {
+    end: {
+      column: number;
+      line: number;
+      offset: number;
+    };
+    start: {
+      column: number;
+      line: number;
+      offset: number;
+    };
+  };
+  raw: string;
+  selector: string;
+};
+
+export type { ClassToStyles, CodemodOptions, Options, Style };
