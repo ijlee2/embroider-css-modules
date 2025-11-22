bd2d3dc096644e33a178bd2cd17e5c73b8cd11a7
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-11-22 07:22:54 +0100

    refactor: Extracted class (utils/get-classes)

--- packages/ember-codemod-remove-global-styles/src/utils/css/get-classes.ts
+++ packages/ember-codemod-remove-global-styles/src/utils/css/get-classes.ts
@@ -1,70 +1,9 @@
 import { AST } from '@codemod-utils/ast-template';
 
-type MustacheStatement = ReturnType<typeof AST.builders.mustache>;
-type StringLiteral = ReturnType<typeof AST.builders.string>;
-type TextNode = ReturnType<typeof AST.builders.text>;
+import { Processor, type ProcessorReturn } from './get-classes/index.js';
 
-export function getClasses(file: string): {
-  classes: string[];
-  errors: string[];
-} {
-  const classes = new Set<string>();
-  const errors: string[] = [];
-
-  function processMustacheStatement(nodeValue: MustacheStatement): void {
-    switch (nodeValue.path.type) {
-      case 'PathExpression': {
-        switch (nodeValue.path.original) {
-          case 'if':
-          case 'unless': {
-            if (nodeValue.params[1]?.type === 'StringLiteral') {
-              processStringLiteral(nodeValue.params[1]);
-            }
-
-            if (nodeValue.params[2]?.type === 'StringLiteral') {
-              processStringLiteral(nodeValue.params[2]);
-            }
-
-            break;
-          }
-
-          default: {
-            const isLocalClass = nodeValue.path.original.startsWith('styles.');
-
-            if (!isLocalClass) {
-              errors.push(
-                `Could not analyze {{${nodeValue.path.original}}} in template, line ${nodeValue.loc.start.line}.`,
-              );
-            }
-          }
-        }
-
-        break;
-      }
-
-      case 'StringLiteral': {
-        processStringLiteral(nodeValue.path);
-        break;
-      }
-    }
-  }
-
-  function processStringLiteral(nodeValue: StringLiteral): void {
-    const classNames = nodeValue.original.split(/\s+/).filter(Boolean);
-
-    classNames.forEach((className) => {
-      classes.add(className);
-    });
-  }
-
-  function processTextNode(nodeValue: TextNode): void {
-    const classNames = nodeValue.chars.split(/\s+/).filter(Boolean);
-
-    classNames.forEach((className) => {
-      classes.add(className);
-    });
-  }
-
+export function getClasses(file: string): ProcessorReturn {
+  const processor = new Processor();
   const traverse = AST.traverse();
 
   traverse(file, {
@@ -78,12 +17,12 @@ export function getClasses(file: string): {
           node.value.parts.forEach((part) => {
             switch (part.type) {
               case 'MustacheStatement': {
-                processMustacheStatement(part);
+                processor.processMustacheStatement(part);
                 break;
               }
 
               case 'TextNode': {
-                processTextNode(part);
+                processor.processTextNode(part);
                 break;
               }
             }
@@ -93,20 +32,17 @@ export function getClasses(file: string): {
         }
 
         case 'MustacheStatement': {
-          processMustacheStatement(node.value);
+          processor.processMustacheStatement(node.value);
           break;
         }
 
         case 'TextNode': {
-          processTextNode(node.value);
+          processor.processTextNode(node.value);
           break;
         }
       }
     },
   });
 
-  return {
-    classes: Array.from(classes),
-    errors,
-  };
+  return processor.print();
 }
