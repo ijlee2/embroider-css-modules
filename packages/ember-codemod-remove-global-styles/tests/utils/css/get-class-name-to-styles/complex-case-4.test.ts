caf38b08d74d75419466941626e861315ea58ae4
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-02-21 09:51:50 +0100

    refactor: Allowed testing getClassNameToStyles on Windows

--- packages/ember-codemod-remove-global-styles/tests/utils/css/get-class-name-to-styles/complex-case-4.test.ts
+++ packages/ember-codemod-remove-global-styles/tests/utils/css/get-class-name-to-styles/complex-case-4.test.ts
@@ -1,77 +1,73 @@
-import { assert, normalizeFile } from '@codemod-utils/tests';
+import { assert, normalizeFile, test } from '@codemod-utils/tests';
 
 import { getClassNameToStyles } from '../../../../src/utils/css/index.js';
-import { testOnPosix } from '../../../helpers/index.js';
 
-testOnPosix(
-  'utils | css | get-class-name-to-styles > complex case (4)',
-  function () {
-    const file = normalizeFile([
-      `.container:not(.is-wide, .no-feedback) {`,
-      `  column-gap: 0;`,
-      `  grid-template-areas:`,
-      `    "label"`,
-      `    "field"`,
-      `    "feedback";`,
-      `  grid-template-columns: 1fr;`,
-      `  grid-template-rows: auto 1fr auto;`,
-      `  row-gap: 0.5rem;`,
-      `}`,
-      ``,
-      `.container.is-inline:not(.is-wide, .no-feedback) {`,
-      `  column-gap: 1rem;`,
-      `  grid-template-areas:`,
-      `    "field label"`,
-      `    "feedback feedback";`,
-      `  grid-template-columns: auto 1fr;`,
-      `  grid-template-rows: 1fr auto;`,
-      `  row-gap: 0.5rem;`,
-      `}`,
-      ``,
-    ]);
+test('utils | css | get-class-name-to-styles > complex case (4)', function () {
+  const file = normalizeFile([
+    `.container:not(.is-wide, .no-feedback) {`,
+    `  column-gap: 0;`,
+    `  grid-template-areas:`,
+    `    "label"`,
+    `    "field"`,
+    `    "feedback";`,
+    `  grid-template-columns: 1fr;`,
+    `  grid-template-rows: auto 1fr auto;`,
+    `  row-gap: 0.5rem;`,
+    `}`,
+    ``,
+    `.container.is-inline:not(.is-wide, .no-feedback) {`,
+    `  column-gap: 1rem;`,
+    `  grid-template-areas:`,
+    `    "field label"`,
+    `    "feedback feedback";`,
+    `  grid-template-columns: auto 1fr;`,
+    `  grid-template-rows: 1fr auto;`,
+    `  row-gap: 0.5rem;`,
+    `}`,
+    ``,
+  ]);
 
-    assert.deepStrictEqual(
-      getClassNameToStyles(file),
-      new Map([
+  assert.deepStrictEqual(
+    getClassNameToStyles(file),
+    new Map([
+      [
+        'container',
         [
-          'container',
-          [
-            {
-              classNames: ['container', 'is-wide', 'no-feedback'],
-              code: normalizeFile([
-                `.container:not(.is-wide, .no-feedback) {`,
-                `  column-gap: 0;`,
-                `  grid-template-areas:`,
-                `    "label"`,
-                `    "field"`,
-                `    "feedback";`,
-                `  grid-template-columns: 1fr;`,
-                `  grid-template-rows: auto 1fr auto;`,
-                `  row-gap: 0.5rem;`,
-                `}`,
-              ]),
-              line: 1,
-              selector: '.container:not(.is-wide, .no-feedback)',
-            },
-            {
-              classNames: ['container', 'is-inline', 'is-wide', 'no-feedback'],
-              code: normalizeFile([
-                `.container.is-inline:not(.is-wide, .no-feedback) {`,
-                `  column-gap: 1rem;`,
-                `  grid-template-areas:`,
-                `    "field label"`,
-                `    "feedback feedback";`,
-                `  grid-template-columns: auto 1fr;`,
-                `  grid-template-rows: 1fr auto;`,
-                `  row-gap: 0.5rem;`,
-                `}`,
-              ]),
-              line: 12,
-              selector: '.container.is-inline:not(.is-wide, .no-feedback)',
-            },
-          ],
+          {
+            classNames: ['container', 'is-wide', 'no-feedback'],
+            code: normalizeFile([
+              `.container:not(.is-wide, .no-feedback) {`,
+              `  column-gap: 0;`,
+              `  grid-template-areas:`,
+              `    "label"`,
+              `    "field"`,
+              `    "feedback";`,
+              `  grid-template-columns: 1fr;`,
+              `  grid-template-rows: auto 1fr auto;`,
+              `  row-gap: 0.5rem;`,
+              `}`,
+            ]),
+            line: 1,
+            selector: '.container:not(.is-wide, .no-feedback)',
+          },
+          {
+            classNames: ['container', 'is-inline', 'is-wide', 'no-feedback'],
+            code: normalizeFile([
+              `.container.is-inline:not(.is-wide, .no-feedback) {`,
+              `  column-gap: 1rem;`,
+              `  grid-template-areas:`,
+              `    "field label"`,
+              `    "feedback feedback";`,
+              `  grid-template-columns: auto 1fr;`,
+              `  grid-template-rows: 1fr auto;`,
+              `  row-gap: 0.5rem;`,
+              `}`,
+            ]),
+            line: 12,
+            selector: '.container.is-inline:not(.is-wide, .no-feedback)',
+          },
         ],
-      ]),
-    );
-  },
-);
+      ],
+    ]),
+  );
+});
--- packages/ember-codemod-remove-global-styles/tests/utils/css/get-class-name-to-styles/simple-case-3.test.ts
+++ packages/ember-codemod-remove-global-styles/tests/utils/css/get-class-name-to-styles/simple-case-3.test.ts
@@ -1,104 +1,100 @@
-import { assert, normalizeFile } from '@codemod-utils/tests';
+import { assert, normalizeFile, test } from '@codemod-utils/tests';
 
 import { getClassNameToStyles } from '../../../../src/utils/css/index.js';
-import { testOnPosix } from '../../../helpers/index.js';
 
-testOnPosix(
-  'utils | css | get-class-name-to-styles > simple case (3)',
-  function () {
-    const file = normalizeFile([
-      `.input {`,
-      `  border: 0.125rem solid #ffd54f;`,
-      `  padding: 0.125rem 0.25rem;`,
-      `  width: calc(100% - 0.75rem);`,
-      `}`,
-      ``,
-      `.input:focus {`,
-      `  background-color: #ffecb3;`,
-      `  outline: 0;`,
-      `}`,
-      ``,
-      `.input:not(:focus) {`,
-      `  border-color: transparent;`,
-      `}`,
-      ``,
-      `.input::placeholder {`,
-      `  font-style: italic;`,
-      `}`,
-      ``,
-      `.is-disabled {`,
-      `  composes: input-disabled from global;`,
-      `}`,
-      ``,
-    ]);
+test('utils | css | get-class-name-to-styles > simple case (3)', function () {
+  const file = normalizeFile([
+    `.input {`,
+    `  border: 0.125rem solid #ffd54f;`,
+    `  padding: 0.125rem 0.25rem;`,
+    `  width: calc(100% - 0.75rem);`,
+    `}`,
+    ``,
+    `.input:focus {`,
+    `  background-color: #ffecb3;`,
+    `  outline: 0;`,
+    `}`,
+    ``,
+    `.input:not(:focus) {`,
+    `  border-color: transparent;`,
+    `}`,
+    ``,
+    `.input::placeholder {`,
+    `  font-style: italic;`,
+    `}`,
+    ``,
+    `.is-disabled {`,
+    `  composes: input-disabled from global;`,
+    `}`,
+    ``,
+  ]);
 
-    assert.deepStrictEqual(
-      getClassNameToStyles(file),
-      new Map([
+  assert.deepStrictEqual(
+    getClassNameToStyles(file),
+    new Map([
+      [
+        'input',
         [
-          'input',
-          [
-            {
-              classNames: ['input'],
-              code: normalizeFile([
-                `.input {`,
-                `  border: 0.125rem solid #ffd54f;`,
-                `  padding: 0.125rem 0.25rem;`,
-                `  width: calc(100% - 0.75rem);`,
-                `}`,
-              ]),
-              line: 1,
-              selector: '.input',
-            },
-            {
-              classNames: ['input'],
-              code: normalizeFile([
-                `.input:focus {`,
-                `  background-color: #ffecb3;`,
-                `  outline: 0;`,
-                `}`,
-              ]),
-              line: 7,
-              selector: '.input:focus',
-            },
-            {
-              classNames: ['input'],
-              code: normalizeFile([
-                `.input:not(:focus) {`,
-                `  border-color: transparent;`,
-                `}`,
-              ]),
-              line: 12,
-              selector: '.input:not(:focus)',
-            },
-            {
-              classNames: ['input'],
-              code: normalizeFile([
-                `.input::placeholder {`,
-                `  font-style: italic;`,
-                `}`,
-              ]),
-              line: 16,
-              selector: '.input::placeholder',
-            },
-          ],
+          {
+            classNames: ['input'],
+            code: normalizeFile([
+              `.input {`,
+              `  border: 0.125rem solid #ffd54f;`,
+              `  padding: 0.125rem 0.25rem;`,
+              `  width: calc(100% - 0.75rem);`,
+              `}`,
+            ]),
+            line: 1,
+            selector: '.input',
+          },
+          {
+            classNames: ['input'],
+            code: normalizeFile([
+              `.input:focus {`,
+              `  background-color: #ffecb3;`,
+              `  outline: 0;`,
+              `}`,
+            ]),
+            line: 7,
+            selector: '.input:focus',
+          },
+          {
+            classNames: ['input'],
+            code: normalizeFile([
+              `.input:not(:focus) {`,
+              `  border-color: transparent;`,
+              `}`,
+            ]),
+            line: 12,
+            selector: '.input:not(:focus)',
+          },
+          {
+            classNames: ['input'],
+            code: normalizeFile([
+              `.input::placeholder {`,
+              `  font-style: italic;`,
+              `}`,
+            ]),
+            line: 16,
+            selector: '.input::placeholder',
+          },
         ],
+      ],
+      [
+        'is-disabled',
         [
-          'is-disabled',
-          [
-            {
-              classNames: ['is-disabled'],
-              code: normalizeFile([
-                `.is-disabled {`,
-                `  composes: input-disabled from global;`,
-                `}`,
-              ]),
-              line: 20,
-              selector: '.is-disabled',
-            },
-          ],
+          {
+            classNames: ['is-disabled'],
+            code: normalizeFile([
+              `.is-disabled {`,
+              `  composes: input-disabled from global;`,
+              `}`,
+            ]),
+            line: 20,
+            selector: '.is-disabled',
+          },
         ],
-      ]),
-    );
-  },
-);
+      ],
+    ]),
+  );
+});
