a50ba29a7b627a0f67ccbead23b4f9b579929077
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-04 10:04:37 +0100

    chore: Patched #232

--- packages/ember-codemod-remove-global-styles/tests/steps/analyze-project/my-v1-app.test.ts
+++ packages/ember-codemod-remove-global-styles/tests/steps/analyze-project/my-v1-app.test.ts
@@ -1,3 +1,5 @@
+import { normalize } from 'node:path';
+
 import { assert, loadFixture, test } from '@codemod-utils/tests';
 
 import { analyzeProject } from '../../../src/steps/index.js';
@@ -12,38 +14,44 @@ test('steps | analyze-project > my-v1-app', function () {
 
   const project = analyzeProject(options);
 
-  assert.deepStrictEqual(Array.from(project.components.keys()), [
-    'app/components/products/product/card.hbs',
-    'app/components/products/product/image.hbs',
-    'app/components/ui/form.hbs',
-    'app/components/ui/form/checkbox.hbs',
-    'app/components/ui/form/field.hbs',
-    'app/components/ui/form/information.hbs',
-    'app/components/ui/form/input.hbs',
-    'app/components/ui/form/number.hbs',
-    'app/components/ui/form/textarea.hbs',
-    'app/components/widgets/widget-1.hbs',
-    'app/components/widgets/widget-1/item.hbs',
-    'app/components/widgets/widget-2.hbs',
-    'app/components/widgets/widget-2/captions.hbs',
-    'app/components/widgets/widget-2/stacked-chart.hbs',
-    'app/components/widgets/widget-3.hbs',
-    'app/components/widgets/widget-3/tour-schedule.hbs',
-    'app/components/widgets/widget-3/tour-schedule/responsive-image.hbs',
-    'app/components/widgets/widget-4.hbs',
-    'app/components/widgets/widget-4/memo.hbs',
-    'app/components/widgets/widget-4/memo/actions.hbs',
-    'app/components/widgets/widget-4/memo/body.hbs',
-    'app/components/widgets/widget-4/memo/header.hbs',
-    'app/components/widgets/widget-5.hbs',
-  ]);
+  assert.deepStrictEqual(
+    Array.from(project.components.keys()),
+    [
+      'app/components/products/product/card.hbs',
+      'app/components/products/product/image.hbs',
+      'app/components/ui/form.hbs',
+      'app/components/ui/form/checkbox.hbs',
+      'app/components/ui/form/field.hbs',
+      'app/components/ui/form/information.hbs',
+      'app/components/ui/form/input.hbs',
+      'app/components/ui/form/number.hbs',
+      'app/components/ui/form/textarea.hbs',
+      'app/components/widgets/widget-1.hbs',
+      'app/components/widgets/widget-1/item.hbs',
+      'app/components/widgets/widget-2.hbs',
+      'app/components/widgets/widget-2/captions.hbs',
+      'app/components/widgets/widget-2/stacked-chart.hbs',
+      'app/components/widgets/widget-3.hbs',
+      'app/components/widgets/widget-3/tour-schedule.hbs',
+      'app/components/widgets/widget-3/tour-schedule/responsive-image.hbs',
+      'app/components/widgets/widget-4.hbs',
+      'app/components/widgets/widget-4/memo.hbs',
+      'app/components/widgets/widget-4/memo/actions.hbs',
+      'app/components/widgets/widget-4/memo/body.hbs',
+      'app/components/widgets/widget-4/memo/header.hbs',
+      'app/components/widgets/widget-5.hbs',
+    ].map(normalize),
+  );
 
-  assert.deepStrictEqual(Array.from(project.routes.keys()), [
-    'app/templates/application.hbs',
-    'app/templates/dashboard.hbs',
-    'app/templates/form.hbs',
-    'app/templates/index.hbs',
-    'app/templates/not-found.hbs',
-    'app/templates/products.hbs',
-  ]);
+  assert.deepStrictEqual(
+    Array.from(project.routes.keys()),
+    [
+      'app/templates/application.hbs',
+      'app/templates/dashboard.hbs',
+      'app/templates/form.hbs',
+      'app/templates/index.hbs',
+      'app/templates/not-found.hbs',
+      'app/templates/products.hbs',
+    ].map(normalize),
+  );
 });
--- packages/ember-codemod-remove-global-styles/tests/steps/analyze-project/my-v2-app.test.ts
+++ packages/ember-codemod-remove-global-styles/tests/steps/analyze-project/my-v2-app.test.ts
@@ -1,3 +1,5 @@
+import { normalize } from 'node:path';
+
 import { assert, loadFixture, test } from '@codemod-utils/tests';
 
 import { analyzeProject } from '../../../src/steps/index.js';
@@ -12,38 +14,44 @@ test('steps | analyze-project > my-v2-app', function () {
 
   const project = analyzeProject(options);
 
-  assert.deepStrictEqual(Array.from(project.components.keys()), [
-    'app/components/products/product/card.gts',
-    'app/components/products/product/image.gts',
-    'app/components/ui/form.gts',
-    'app/components/ui/form/checkbox.gts',
-    'app/components/ui/form/field.gts',
-    'app/components/ui/form/information.gts',
-    'app/components/ui/form/input.gts',
-    'app/components/ui/form/number.gts',
-    'app/components/ui/form/textarea.gts',
-    'app/components/widgets/widget-1.gts',
-    'app/components/widgets/widget-1/item.gjs',
-    'app/components/widgets/widget-2.gts',
-    'app/components/widgets/widget-2/captions.gts',
-    'app/components/widgets/widget-2/stacked-chart.gts',
-    'app/components/widgets/widget-3.gts',
-    'app/components/widgets/widget-3/tour-schedule.gjs',
-    'app/components/widgets/widget-3/tour-schedule/responsive-image.gjs',
-    'app/components/widgets/widget-4.gjs',
-    'app/components/widgets/widget-4/memo.gts',
-    'app/components/widgets/widget-4/memo/actions.gts',
-    'app/components/widgets/widget-4/memo/body.gts',
-    'app/components/widgets/widget-4/memo/header.gts',
-    'app/components/widgets/widget-5.gts',
-  ]);
+  assert.deepStrictEqual(
+    Array.from(project.components.keys()),
+    [
+      'app/components/products/product/card.gts',
+      'app/components/products/product/image.gts',
+      'app/components/ui/form.gts',
+      'app/components/ui/form/checkbox.gts',
+      'app/components/ui/form/field.gts',
+      'app/components/ui/form/information.gts',
+      'app/components/ui/form/input.gts',
+      'app/components/ui/form/number.gts',
+      'app/components/ui/form/textarea.gts',
+      'app/components/widgets/widget-1.gts',
+      'app/components/widgets/widget-1/item.gjs',
+      'app/components/widgets/widget-2.gts',
+      'app/components/widgets/widget-2/captions.gts',
+      'app/components/widgets/widget-2/stacked-chart.gts',
+      'app/components/widgets/widget-3.gts',
+      'app/components/widgets/widget-3/tour-schedule.gjs',
+      'app/components/widgets/widget-3/tour-schedule/responsive-image.gjs',
+      'app/components/widgets/widget-4.gjs',
+      'app/components/widgets/widget-4/memo.gts',
+      'app/components/widgets/widget-4/memo/actions.gts',
+      'app/components/widgets/widget-4/memo/body.gts',
+      'app/components/widgets/widget-4/memo/header.gts',
+      'app/components/widgets/widget-5.gts',
+    ].map(normalize),
+  );
 
-  assert.deepStrictEqual(Array.from(project.routes.keys()), [
-    'app/templates/application.gts',
-    'app/templates/dashboard.gjs',
-    'app/templates/form.gts',
-    'app/templates/index.gjs',
-    'app/templates/not-found.gts',
-    'app/templates/products.gts',
-  ]);
+  assert.deepStrictEqual(
+    Array.from(project.routes.keys()),
+    [
+      'app/templates/application.gts',
+      'app/templates/dashboard.gjs',
+      'app/templates/form.gts',
+      'app/templates/index.gjs',
+      'app/templates/not-found.gts',
+      'app/templates/products.gts',
+    ].map(normalize),
+  );
 });
