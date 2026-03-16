55307ee46f6c162ddb1e6559b6547ddb58eef9f7
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-16 12:18:36 +0100

    feature: Improved handling of nested selectors

--- packages/type-css-modules/src/utils/css/get-class-names.ts
+++ packages/type-css-modules/src/utils/css/get-class-names.ts
@@ -1,29 +1,71 @@
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
+import { type AstRule, createParser, traverse } from 'css-selector-parser';
+import type { Plugin } from 'postcss';
+import postcss from 'postcss';
+
 import type { Options } from '../../types/index.js';
-import { ASTCSS as AST } from '../ast.js';
+
+const parse = createParser({
+  // Allow unknown pseudo-classes
+  syntax: 'progressive',
+});
+
+function parseRule(rule: AstRule): string[] {
+  const classNames: string[] = [];
+
+  traverse(rule, (node) => {
+    if (node.type === 'ClassName') {
+      classNames.push(node.name);
+    }
+  });
+
+  return classNames;
+}
+
+function parseSelector(maybeSelector: string): string[] {
+  const classNames: string[] = [];
+
+  try {
+    const { rules } = parse(maybeSelector);
+
+    rules.forEach((rule) => {
+      traverse(rule, (node) => {
+        if (node.type === 'Rule') {
+          classNames.push(...parseRule(node));
+        }
+      });
+    });
+  } catch {
+    // Do nothing
+  }
+
+  return classNames;
+}
 
 export function getClassNames(filePath: string, options: Options): string[] {
   const { projectRoot } = options;
+
   const file = readFileSync(join(projectRoot, filePath), 'utf8');
+  const classNames: string[] = [];
 
-  const traverse = AST.traverse();
-  const classNames = new Set<string>();
-
-  traverse(file, {
-    ClassSelector(node) {
-      classNames.add(node.name);
+  const plugins: Plugin[] = [
+    {
+      postcssPlugin: 'postcss-get-class-names',
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      prepare() {
+        return {
+          Rule(node): void {
+            classNames.push(...parseSelector(node.selector));
+          },
+        };
+      },
     },
+  ];
 
-    PseudoClassSelector(node) {
-      if (node.name === 'local') {
-        console.warn(
-          `WARNING: type-css-modules assumes that all user-defined classes are local. Consider removing the pseudo-class :local() in \`${filePath}\`.`,
-        );
-      }
-    },
-  });
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  postcss(plugins).process(file).css;
 
-  return Array.from(classNames).sort();
+  return Array.from(new Set(classNames)).sort();
 }
