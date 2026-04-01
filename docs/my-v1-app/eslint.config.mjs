6fd4d99b7af67046b81d0c024f734db0c492b115
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-04-01 14:18:55 +0200

    chore: Replaced @tsconfig/ember with @ember/app-tsconfig and @ember/library-tsconfig

--- docs/my-v1-app/eslint.config.mjs
+++ docs/my-v1-app/eslint.config.mjs
@@ -1,11 +1,1 @@
-import baseConfiguration from '@shared-configs/eslint-config-ember/v1-app';
-
-export default [
-  ...baseConfiguration,
-  {
-    files: ['**/*.{gts,ts}'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  },
-];
+export { default } from '@shared-configs/eslint-config-ember/v1-app';
