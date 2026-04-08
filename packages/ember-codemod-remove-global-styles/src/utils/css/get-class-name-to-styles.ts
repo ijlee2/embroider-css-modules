7df51e9d47d8a4a9568ccbbb0c1b914f3e34fa98
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-04-08 16:10:37 +0200

    refactor: Allowed native types

--- packages/ember-codemod-remove-global-styles/src/utils/css/get-class-name-to-styles.ts
+++ packages/ember-codemod-remove-global-styles/src/utils/css/get-class-name-to-styles.ts
@@ -9,25 +9,6 @@ import postcss from 'postcss';
 
 import type { ClassNameToStyles } from '../../types/index.js';
 
-type Node = {
-  [key: string]: unknown;
-  clone: () => Node;
-  selector: string;
-  source: {
-    end: {
-      column: number;
-      line: number;
-      offset: number;
-    };
-    start: {
-      column: number;
-      line: number;
-      offset: number;
-    };
-  };
-  toString: () => string;
-};
-
 type Selector = {
   childClassNames: string[];
   parentClassName: string;
@@ -92,38 +73,37 @@ function parseSelector(maybeSelector: string): Selector[] {
 export function getClassNameToStyles(file: string): ClassNameToStyles {
   const classNameToStyles: ClassNameToStyles = new Map();
 
-  function processRule(node: Node): void {
-    const clone = node.clone();
-    const line = node.source.start.line;
-
-    const selectors = parseSelector(node.selector);
-
-    selectors.forEach(({ childClassNames, parentClassName, selector }) => {
-      clone.selector = selector;
-
-      const style = {
-        classNames: childClassNames,
-        code: clone.toString(),
-        line,
-        selector,
-      };
-
-      if (classNameToStyles.has(parentClassName)) {
-        classNameToStyles.get(parentClassName)!.push(style);
-      } else {
-        classNameToStyles.set(parentClassName, [style]);
-      }
-    });
-  }
-
   const plugins: Plugin[] = [
     {
       postcssPlugin: 'postcss-get-class-name-to-styles',
-      // @ts-expect-error: Incorrect type
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       prepare() {
         return {
-          Rule: processRule,
+          Rule(node): void {
+            const clone = node.clone();
+            const line = node.source!.start!.line;
+
+            const selectors = parseSelector(node.selector);
+
+            selectors.forEach(
+              ({ childClassNames, parentClassName, selector }) => {
+                clone.selector = selector;
+
+                const style = {
+                  classNames: childClassNames,
+                  code: clone.toString(),
+                  line,
+                  selector,
+                };
+
+                if (classNameToStyles.has(parentClassName)) {
+                  classNameToStyles.get(parentClassName)!.push(style);
+                } else {
+                  classNameToStyles.set(parentClassName, [style]);
+                }
+              },
+            );
+          },
         };
       },
     },
